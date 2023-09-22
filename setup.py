2f4f7f007e30f0e1fe08e415eca865fc71316964
Trim Bresilla <trim.bresilla@gmail.com>
2023-09-22 11:29:08 +0200

    feat: Add support for RTK navigation
    - Update setup.py to include new items in the data_files parameter
    - Add rtk_navigation/utils.py with various functions for GPS and local coordinate conversions, distance calculations, and coordinate rotation
    - Add navigation.launch.py with launch dependencies, arguments, and nodes for rtk_navigation
    - Add rtk_navigation/path_server.py with classes for handling positioning data and movement, implemented callbacks, and navigation parameters retrieval

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
-from setuptools import find_packages, setup
-
+from glob import glob
+from setuptools import setup
+import os
 package_name = 'rtk_navigation'
+
+data_files = []
+#resources
+data_files.append(('share/' + package_name + '/resource', glob('resource/*')))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+#launch files
+data_files.append((os.path.join('share', package_name), glob('launch/*.py')))
+#params
+data_files.append(('share/' + package_name + '/params', glob('params/*')))
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    packages=[package_name],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='bresilla',
@@ -20,7 +27,11 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rtk_navigation = rtk_navigation.rtk_navigation:main'
+            'rtk_navigation = rtk_navigation.rtk_navigation:main',
+            'path_server = rtk_navigation.path_server:main'
         ],
+        'launch.frontend.launch_extension': [
+            'launch_ros = launch_ros'
+        ]
     },
 )
